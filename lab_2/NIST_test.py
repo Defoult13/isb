6627041ff0dbc78271ad3ea9a5ef154314abbf52
aidenpirs007@mail.ru <aidenpirs007@mail.ru>
2024-04-28 21:10:25 +0400

    Created consec test

--- lab_2/NIST_test.py
+++ lab_2/NIST_test.py
@@ -8,3 +8,12 @@ def bit_frequency_test(sequence):
     P_value = math.erfc(Sn / math.sqrt(2))
     return P_value
 
+def consecutive_bit_test(sequence):
+    n = len(sequence)
+    y = sum(int(bit) for bit in sequence) / n
+    if abs(y - 1/2) >= 2 / math.sqrt(n):
+        return 0
+    Vn = sum(1 for i in range(n - 1) if sequence[i] != sequence[i + 1])
+    P_value = math.erfc(abs(Vn - 2 * n * y * (1 - y)) / (2 * math.sqrt(2 * n) * y * (1 - y)))
+    return P_value
+
