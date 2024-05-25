0407a9d10becdbfc4322484c587f02135fc27f1c
aidenpirs007@mail.ru <aidenpirs007@mail.ru>
2024-05-25 22:00:26 +0400

    PEP fix

--- lab_3/main.py
+++ lab_3/main.py
@@ -87,6 +87,7 @@ def main(config_path, operation):
         case _:
             logging.error(f"Unknown operation: {config['operation']}")
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Process some cryptographic operations.")
     parser.add_argument("--config_path", type=str, default= os.path.join("K", "Pyth", "ISB", "isb", "lab_3", "config.json"), help="Path to the JSON configuration file.")
